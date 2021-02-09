e49a78b88f38181bc106fba3443b4af36002a8c5
Iq8x7rm <47464956+Iq8x7rm@users.noreply.github.com>
2021-02-09 11:12:24 -0600

    Update ClientGUI.java

--- CustomerServiceChat/CustomerServiceClient/src/main/java/ClientGUI.java
+++ CustomerServiceChat/CustomerServiceClient/src/main/java/ClientGUI.java
@@ -91,7 +91,7 @@ public class ClientGUI extends Application {
 		// If click enter button, typed up message will send
 		Scene communicationScene = new Scene(ClientGUIElements, 400, 600);
 		communicationScene.setOnKeyPressed(a -> {
-			if (a.getCode() == KeyCode.ENTER) {
+			if (a.getCode() == KeyCode.ENTER && sendMessage.isDisabled() == fale) {
 				theClient.sendMessage(message.getText());
 				message.clear();
 			}
